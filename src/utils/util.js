6c84460802f2cc2b90edaebc5afd59323bd23b0f
semdy <501859275@qq.com>
2018-08-13 17:55:36 +0800

    任务盘点功能完善

--- src/utils/util.js
+++ src/utils/util.js
@@ -25,12 +25,36 @@ function formatDate (source, format) {
   return format
 }
 
-function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-  })
-}
+const uuid = (function () {
+  let CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
+  return function (len, radix) {
+    var chars = CHARS, uuid = [], i
+    radix = radix || chars.length
+
+    if (len) {
+      // Compact form
+      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix]
+    } else {
+      // rfc4122, version 4 form
+      let r
+
+      // rfc4122 requires these characters
+      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-'
+      uuid[14] = '4'
+
+      // Fill in random data.  At i==19 set the high bits of clock sequence as
+      // per rfc4122, sec. 4.1.5
+      for (i = 0; i < 36; i++) {
+        if (!uuid[i]) {
+          r = 0 | Math.random() * 16
+          uuid[i] = chars[(i === 19) ? (r & 0x3) | 0x8 : r]
+        }
+      }
+    }
+
+    return uuid.join('')
+  }
+})()
 
 function showError (msg, duration) {
   duration = duration || 1500;
@@ -38,7 +62,8 @@ function showError (msg, duration) {
     setTimeout(function(){
       wx.showToast({
         title: msg,
-        image: '../../images/error.png',
+        //image: '../../images/error.png',
+        icon: 'none',
         duration: duration
       })
       setTimeout(resolve, duration)
