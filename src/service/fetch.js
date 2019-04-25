47a0b7ea93466a30924707f790f49a669ccc84a1
semdy <501859275@qq.com>
2019-04-25 09:58:09 +0800

    退出控制机制修改

--- src/service/fetch.js
+++ src/service/fetch.js
@@ -4,15 +4,16 @@ import {showError, uuid, redirectToLogin} from '../utils/util'
 import {version, ref} from '../config'
 import {session} from '../service/auth'
 
-export const serverUrl = 'https://bscqr.qtdatas.com/api/'
+export const serverUrl = 'https://bscqr.qtdatas.com/'
 // export const serverUrl = 'https://bscqr.qtdatas.com/dev/'
 
+let isLogout = false
 const logout = () => {
-  if (wx.getStorageSync('__ISLOGOUT')) return
+  if (isLogout) return
   session.clear()
   wx.removeStorageSync('needRefresh.home')
   redirectToLogin()
-  wx.setStorageSync('__ISLOGOUT', '1')
+  isLogout = true
 }
 
 let requestCount = 0
@@ -21,11 +22,13 @@ let errorMsg = ''
 let fetchApi = (url, params = {}, useToken = true, showLoading = true) => {
   return new Promise((resolve, reject) => {
 
-    showLoading && requestCount++
+    if (showLoading) {
+      requestCount++
+    }
     errorMsg = ''
 
     if (requestCount === 1) {
-      showLoading && wx.showLoading({
+      wx.showLoading({
         mask: true,
         title: '请稍候...'
       })
@@ -51,18 +54,19 @@ let fetchApi = (url, params = {}, useToken = true, showLoading = true) => {
     }
 
     wepy.request({
-      url: `${serverUrl}${url}?version=${version}`,
+      url: `${serverUrl}api/${url}?version=${version}`,
       data: Object.assign({}, params.data, params.method === 'POST' && {ref}),
       method: params.method || 'GET',
       header: Object.assign(defHeaders, params.header)
     })
     .then(res => {
       if (res.statusCode === 200) {
-        if (res.data.tokenValid === false) {
+        if (res.data.tokenValid) {
+          isLogout = false
+          resolve(res.data)
+        } else {
           logout()
           reject('登录信息过期')
-        } else {
-          resolve(res.data)
         }
       } else {
         reject(errorMsg = (res.data.message || '服务器发生错误'))
@@ -72,12 +76,14 @@ let fetchApi = (url, params = {}, useToken = true, showLoading = true) => {
       reject(errorMsg = '与服务器连接失败')
     })
     .finally(() => {
-      showLoading && requestCount--
+      if (showLoading) {
+        requestCount--
+      }
       if (requestCount === 0) {
         if (errorMsg) {
           showError(errorMsg)
         } else {
-          showLoading && wx.hideLoading()
+          wx.hideLoading()
         }
         wx.stopPullDownRefresh()
       }
